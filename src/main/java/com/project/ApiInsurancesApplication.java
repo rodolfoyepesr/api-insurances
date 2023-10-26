bdc67168b757b2d1df50abcc86274dc241a6e84d
rodolfoyepesr <rodolfo.yepes.rincones@gmail.com>
2023-10-25 21:08:38 -0500

    Fix pom

--- src/main/java/com/project/ApiInsurancesApplication.java
+++ src/main/java/com/project/ApiInsurancesApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiProductsApplication {
+public class ApiInsurancesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiProductsApplication.class, args);
+		SpringApplication.run(ApiInsurancesApplication.class, args);
 	}
 }
